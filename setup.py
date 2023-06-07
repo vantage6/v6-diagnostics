b32956fad89d952995319918d50c975f3795c9ed
Frank Martin <f.martin@iknl.nl>
2023-06-07 15:53:10 +0200

    Removed poetry

---
+++ setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from codecs import open
+from setuptools import setup, find_packages
+
+# we're using a README.md, if you do not have this in your folder, simply
+# replace this with a string.
+here = Path(__file__).parent.resolve()
+with open(here / 'README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+# Here you specify the meta-data of your package. The `name` argument is
+# needed in some other steps.
+setup(
+    name='v6-diagnostics',
+    version="0.1.0",
+    description='vantage6 node diagnostic tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vantage6/v6-diagnostics',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'vantage6-client',
+        'requests',
+        'jwt',
+    ]
+)
