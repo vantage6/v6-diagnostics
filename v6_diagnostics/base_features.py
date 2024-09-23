c525d3c578460bb84f58f7e28bc6ceabfa785dca
Frank Martin <frank@grafikus.nl>
2024-09-23 15:43:27 +0200

    Replaced `get_env_var` with algorithm tools version

--- v6_diagnostics/base_features.py
+++ v6_diagnostics/base_features.py
@@ -46,6 +46,7 @@ does not use any wrapper functions. The following features are tested:
     Database readable
         Check if the file-based database is readable.
 """
+
 # TODO: check that the temporary volume is readable and writable by the
 #       child algorithm container.
 # TODO: child container should trigger different function
@@ -58,26 +59,27 @@ from pathlib import Path
 from requests.exceptions import ConnectionError
 from vantage6.algorithm.client import AlgorithmClient
 
-from v6_diagnostics.util import DiagnosticResult, header, get_env_var
+from v6_diagnostics.util import DiagnosticResult, header
+from vantage6.algorithm.tools.util import get_env_var
 
 
 def diagnose_environment() -> DiagnosticResult:
     """Diagnose the environment of the algorithm container."""
-    header('Diagnose the environment of the algorithm container')
-    diagnostic = DiagnosticResult('ENVIRONMENT', True, os.environ)
+    header("Diagnose the environment of the algorithm container")
+    diagnostic = DiagnosticResult("ENVIRONMENT", True, os.environ)
     print(diagnostic)
     return diagnostic
 
 
 def diagnose_input_file() -> DiagnosticResult:
     """Diagnose the input file."""
-    header('Diagnose the input file')
+    header("Diagnose the input file")
     try:
-        with open(get_env_var('INPUT_FILE'), 'rb') as f:
+        with open(get_env_var("INPUT_FILE"), "rb") as f:
             input_ = f.read()
-        diagnostic = DiagnosticResult('INPUT_FILE', True, input_)
+        diagnostic = DiagnosticResult("INPUT_FILE", True, input_)
     except Exception as exc:
-        diagnostic = DiagnosticResult('INPUT_FILE', False, exception=exc)
+        diagnostic = DiagnosticResult("INPUT_FILE", False, exception=exc)
 
     print(diagnostic)
     return diagnostic
@@ -85,18 +87,18 @@ def diagnose_input_file() -> DiagnosticResult:
 
 def diagnose_output_file() -> DiagnosticResult:
     """Diagnose the output file."""
-    header('Diagnose the output file')
-    test_word = 'test'
+    header("Diagnose the output file")
+    test_word = "test"
     try:
-        with open(get_env_var('OUTPUT_FILE'), 'w') as f:
+        with open(get_env_var("OUTPUT_FILE"), "w") as f:
             f.write(test_word)
 
-        with open(get_env_var('OUTPUT_FILE'), 'r') as f:
+        with open(get_env_var("OUTPUT_FILE"), "r") as f:
             success = f.read() == test_word
 
-        diagnostic = DiagnosticResult('OUTPUT_FILE', success)
+        diagnostic = DiagnosticResult("OUTPUT_FILE", success)
     except Exception as exc:
-        diagnostic = DiagnosticResult('OUTPUT_FILE', False, exception=exc)
+        diagnostic = DiagnosticResult("OUTPUT_FILE", False, exception=exc)
 
     print(diagnostic)
     return diagnostic
@@ -104,13 +106,13 @@ def diagnose_output_file() -> DiagnosticResult:
 
 def diagnose_token_file() -> DiagnosticResult:
     """Diagnose the token file."""
-    header('Diagnose the token file')
+    header("Diagnose the token file")
     try:
-        with open(get_env_var('TOKEN_FILE'), 'r') as f:
+        with open(get_env_var("TOKEN_FILE"), "r") as f:
             token = f.read()
-        diagnostic = DiagnosticResult('TOKEN_FILE', True, token)
+        diagnostic = DiagnosticResult("TOKEN_FILE", True, token)
     except Exception as exc:
-        diagnostic = DiagnosticResult('TOKEN_FILE', False, exception=exc)
+        diagnostic = DiagnosticResult("TOKEN_FILE", False, exception=exc)
 
     print(diagnostic)
     return diagnostic
@@ -118,14 +120,14 @@ def diagnose_token_file() -> DiagnosticResult:
 
 def diagnose_temporary_volume() -> DiagnosticResult:
     """Diagnose the temporary volume."""
-    header('Diagnose writing to temporary volume')
+    header("Diagnose writing to temporary volume")
     try:
-        temp_file = Path(get_env_var("TEMPORARY_FOLDER")) / 'test.txt'
-        with open(temp_file, 'w') as f:
-            f.write('test')
-        diagnostic = DiagnosticResult('TEMPORARY_VOLUME', True)
+        temp_file = Path(get_env_var("TEMPORARY_FOLDER")) / "test.txt"
+        with open(temp_file, "w") as f:
+            f.write("test")
+        diagnostic = DiagnosticResult("TEMPORARY_VOLUME", True)
     except Exception as exc:
-        diagnostic = DiagnosticResult('TEMPORARY_VOLUME', False, exception=exc)
+        diagnostic = DiagnosticResult("TEMPORARY_VOLUME", False, exception=exc)
 
     print(diagnostic)
     return diagnostic
@@ -133,15 +135,15 @@ def diagnose_temporary_volume() -> DiagnosticResult:
 
 def diagnose_temporary_volume_file_exists() -> DiagnosticResult:
     """Diagnose the temporary volume."""
-    header('Diagnose that the temporary file is created')
+    header("Diagnose that the temporary file is created")
     try:
-        temp_file = Path(get_env_var("TEMPORARY_FOLDER")) / 'test.txt'
+        temp_file = Path(get_env_var("TEMPORARY_FOLDER")) / "test.txt"
         file_exists = Path(temp_file).exists()
-        diagnostic = DiagnosticResult('TEMPORARY_VOLUME_FILE_EXISTS',
-                                      file_exists)
+        diagnostic = DiagnosticResult("TEMPORARY_VOLUME_FILE_EXISTS", file_exists)
     except Exception as exc:
-        diagnostic = DiagnosticResult('TEMPORARY_VOLUME_FILE_EXISTS', False,
-                                      exception=exc)
+        diagnostic = DiagnosticResult(
+            "TEMPORARY_VOLUME_FILE_EXISTS", False, exception=exc
+        )
 
     print(diagnostic)
     return diagnostic
@@ -149,15 +151,14 @@ def diagnose_temporary_volume_file_exists() -> DiagnosticResult:
 
 def diagnose_local_proxy() -> DiagnosticResult:
     """Diagnose the local proxy."""
-    header('Diagnose the local proxy')
+    header("Diagnose the local proxy")
     try:
-        host = get_env_var('HOST')
-        port = get_env_var('PORT')
-        response = requests.get(f'{host}:{port}/version')
-        diagnostic = DiagnosticResult('LOCAL_PROXY',
-                                      response.status_code == 200)
+        host = get_env_var("HOST")
+        port = get_env_var("PORT")
+        response = requests.get(f"{host}:{port}/version")
+        diagnostic = DiagnosticResult("LOCAL_PROXY", response.status_code == 200)
     except Exception as exc:
-        diagnostic = DiagnosticResult('LOCAL_PROXY', False, exception=exc)
+        diagnostic = DiagnosticResult("LOCAL_PROXY", False, exception=exc)
 
     print(diagnostic)
     return diagnostic
@@ -165,33 +166,28 @@ def diagnose_local_proxy() -> DiagnosticResult:
 
 def diagnose_local_proxy_subtask(client: AlgorithmClient) -> DiagnosticResult:
     """Diagnose the local proxy."""
-    header('Diagnose the local proxy subtask')
+    header("Diagnose the local proxy subtask")
     try:
 
-        with open(get_env_var('TOKEN_FILE'), 'r') as f:
+        with open(get_env_var("TOKEN_FILE"), "r") as f:
             token = f.read()
 
-        identity = (
-            jwt.decode(token, options={"verify_signature": False})['sub']
-        )
+        identity = jwt.decode(token, options={"verify_signature": False})["sub"]
 
-        input_ = {
-            'master': True,
-            'method': 'diagnose_local_proxy_subtask_stop'
-        }
+        input_ = {"master": True, "method": "diagnose_local_proxy_subtask_stop"}
 
         task = client.task.create(
-            name='feature-tester-subtask',
-            description='This task is from the feature tester',
-            organizations=[identity.get('organization_id')],
-            input_=input_
+            name="feature-tester-subtask",
+            description="This task is from the feature tester",
+            organizations=[identity.get("organization_id")],
+            input_=input_,
         )
 
-        result = client.wait_for_results(task.get('id'))
+        result = client.wait_for_results(task.get("id"))
 
-        diagnostic = DiagnosticResult('CREATE_SUBTASK', result)
+        diagnostic = DiagnosticResult("CREATE_SUBTASK", result)
     except Exception as exc:
-        diagnostic = DiagnosticResult('CREATE_SUBTASK', False, exception=exc)
+        diagnostic = DiagnosticResult("CREATE_SUBTASK", False, exception=exc)
 
     print(diagnostic)
     return diagnostic
@@ -203,42 +199,42 @@ def diagnose_local_proxy_subtask_stop(*_args, **_kwargs) -> bool:
 
 
 def diagnose_isolation() -> DiagnosticResult:
-    header('Diagnose the isolation of the algorithm container')
+    header("Diagnose the isolation of the algorithm container")
     try:
-        requests.get('https://google.nl')
+        requests.get("https://google.nl")
     except ConnectionError:
-        diagnostic = DiagnosticResult('ISOLATION', True)
+        diagnostic = DiagnosticResult("ISOLATION", True)
         print(diagnostic)
         return diagnostic
     except Exception as exc:
         # We could end up here by some other error. This does not necessary
         # mean that the algorithm is not isolated.
-        diagnostic = DiagnosticResult('ISOLATION', False, exception=exc)
+        diagnostic = DiagnosticResult("ISOLATION", False, exception=exc)
         print(diagnostic)
         return diagnostic
 
     # If we get here, we have a connection to the internet
-    diagnostic = DiagnosticResult('ISOLATION', False)
+    diagnostic = DiagnosticResult("ISOLATION", False)
     print(diagnostic)
     return diagnostic
 
 
 def diagnose_external_port() -> DiagnosticResult:
     """Diagnose the external port."""
-    header('Diagnose the external port')
+    header("Diagnose the external port")
     try:
-        with open(get_env_var('TOKEN_FILE'), 'r') as f:
+        with open(get_env_var("TOKEN_FILE"), "r") as f:
             token = f.read()
 
-        host = get_env_var('HOST')
-        port = get_env_var('PORT')
+        host = get_env_var("HOST")
+        port = get_env_var("PORT")
 
         # port should be published as we are running this code.. So no
         # need for polling
         response = requests.get(
-            f'{host}:{port}/vpn/algorithm/addresses',
-            headers={'Authorization': 'Bearer ' + token},
-            params={'include_parent': True, 'include_children': True}
+            f"{host}:{port}/vpn/algorithm/addresses",
+            headers={"Authorization": "Bearer " + token},
+            params={"include_parent": True, "include_children": True},
         )
 
         # we also assume that only a single task has been posted as we
@@ -246,19 +242,19 @@ def diagnose_external_port() -> DiagnosticResult:
         p5 = p8 = False
         pU = True
         result = response.json()
-        for addr in result['addresses']:
-            if addr['label'] == 'port5':
+        for addr in result["addresses"]:
+            if addr["label"] == "port5":
                 p5 = True
-            elif addr['label'] == 'port8':
+            elif addr["label"] == "port8":
                 p8 = True
             else:
                 pU = False
 
-        diagnostic = DiagnosticResult('EXTERNAL_PORT_TEST', all([p5, p8, pU]),
-                                      payload=result)
+        diagnostic = DiagnosticResult(
+            "EXTERNAL_PORT_TEST", all([p5, p8, pU]), payload=result
+        )
     except Exception as exc:
-        diagnostic = DiagnosticResult('EXTERNAL_PORT_TEST', False,
-                                      exception=exc)
+        diagnostic = DiagnosticResult("EXTERNAL_PORT_TEST", False, exception=exc)
 
     print(diagnostic)
     return diagnostic
@@ -266,30 +262,29 @@ def diagnose_external_port() -> DiagnosticResult:
 
 def diagnose_database() -> list[DiagnosticResult]:
     """Diagnose the file-based database."""
-    header('Diagnose the file-based database')
+    header("Diagnose the file-based database")
     diagnostics = []
     try:
-        db_labels = get_env_var('DB_LABELS').split(',')
+        db_labels = get_env_var("DB_LABELS").split(",")
         for label in db_labels:
-            db_uri = get_env_var(f'{label.upper()}_DATABASE_URI')
-            db_type = get_env_var(f'{label.upper()}_DATABASE_TYPE')
+            db_uri = get_env_var(f"{label.upper()}_DATABASE_URI")
+            db_type = get_env_var(f"{label.upper()}_DATABASE_TYPE")
 
-            if db_type in ['sql', 'omop', 'sparql']:
+            if db_type in ["sql", "omop", "sparql"]:
                 # We do not expect these databases to be files, so don't
                 # perform checks on them
                 continue
             elif Path(db_uri).exists():
-                diagnostic = DiagnosticResult(
-                    f'DATABASE {label.upper()}', True
-                )
+                diagnostic = DiagnosticResult(f"DATABASE {label.upper()}", True)
             else:
                 diagnostic = DiagnosticResult(
-                    f'DATABASE {label.upper()}', False,
-                    payload=f"{db_uri} does not exist"
+                    f"DATABASE {label.upper()}",
+                    False,
+                    payload=f"{db_uri} does not exist",
                 )
             diagnostics.append(diagnostic)
     except Exception as exc:
-        diagnostic = DiagnosticResult('DATABASE', False, exception=exc)
+        diagnostic = DiagnosticResult("DATABASE", False, exception=exc)
         diagnostics.append(diagnostic)
 
     for diagnostic in diagnostics:
--- v6_diagnostics/util.py
+++ v6_diagnostics/util.py
@@ -1,4 +1,3 @@
-
 import traceback
 from typing import Any
 import os
@@ -10,8 +9,9 @@ from vantage6.common.globals import STRING_ENCODING, ENV_VAR_EQUALS_REPLACEMENT
 class DiagnosticResult:
     """Class to store the results of a diagnostic test."""
 
-    def __init__(self, name: str, success: bool, payload: Any = None,
-                 exception: Exception = None) -> None:
+    def __init__(
+        self, name: str, success: bool, payload: Any = None, exception: Exception = None
+    ) -> None:
         self.name = name
         self.success = success
         self.payload = payload
@@ -22,15 +22,15 @@ class DiagnosticResult:
     def json(self):
         """Return a JSON representation of the result."""
         return {
-            'name': self.name,
-            'success': self.success,
-            'payload': str(self.payload),
-            'exception': str(self.exception),
-            'traceback': self.traceback
+            "name": self.name,
+            "success": self.success,
+            "payload": str(self.payload),
+            "exception": str(self.exception),
+            "traceback": self.traceback,
         }
 
     def __str__(self):
-        return f'{self.name}: {self.success}'
+        return f"{self.name}: {self.success}"
 
     def __repr__(self):
         return str(self)
@@ -38,32 +38,32 @@ class DiagnosticResult:
 
 def header(text):
     """Print header."""
-    print('\n' + text.center(80, '-'))
+    print("\n" + text.center(80, "-"))
 
 
-def get_env_var(var_name: str, default: str | None = None) -> str:
-    """
-    Get the value of an environment variable. Environment variables are encoded
-    by the node so they need to be decoded here.
+# def get_env_var(var_name: str, default: str | None = None) -> str:
+#     """
+#     Get the value of an environment variable. Environment variables are encoded
+#     by the node so they need to be decoded here.
 
-    Note that this decoding follows the reverse of the encoding in the node:
-    first replace '=' back and then decode the base32 string.
+#     Note that this decoding follows the reverse of the encoding in the node:
+#     first replace '=' back and then decode the base32 string.
 
-    Parameters
-    ----------
-    var_name : str
-        Name of the environment variable
+#     Parameters
+#     ----------
+#     var_name : str
+#         Name of the environment variable
 
-    Returns
-    -------
-    var_value : str | None
-        Value of the environment variable, or default value if not found
-    """
-    try:
-        encoded_env_var_value = \
-            os.environ[var_name].replace(
-                ENV_VAR_EQUALS_REPLACEMENT, "="
-            ).encode(STRING_ENCODING)
-        return base64.b32decode(encoded_env_var_value).decode(STRING_ENCODING)
-    except KeyError:
-        return default
+#     Returns
+#     -------
+#     var_value : str | None
+#         Value of the environment variable, or default value if not found
+#     """
+#     try:
+#         encoded_env_var_value = \
+#             os.environ[var_name].replace(
+#                 ENV_VAR_EQUALS_REPLACEMENT, "="
+#             ).encode(STRING_ENCODING)
+#         return base64.b32decode(encoded_env_var_value).decode(STRING_ENCODING)
+#     except KeyError:
+#         return default
